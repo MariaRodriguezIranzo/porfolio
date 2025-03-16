cfadd9c41d99ca8028673cec50af7861a83a7df0
María Rodríguez Iranzo <mariarodira@campus.monlau.com>
2025-03-16 03:27:27 +0100

    bug navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { Menu, X, Moon, Sun } from "lucide-react";
-import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function Navbar() {
